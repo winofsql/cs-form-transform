c5510f51737e95c14c031dfdfe4339665f84b4dd
sworc <lightbox@nifty.com>
2022-09-04 20:52:18 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace cs_form_transform;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+        this.SuspendLayout();
+        //
+        // richTextBox1
+        //
+        this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+        this.richTextBox1.Font = new System.Drawing.Font("Meiryo UI", 16, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+        this.richTextBox1.Location = new System.Drawing.Point(22, 19);
+        this.richTextBox1.Name = "richTextBox1";
+        this.richTextBox1.Size = new System.Drawing.Size(340, 200);
+        this.richTextBox1.TabIndex = 1;
+        this.richTextBox1.Text = "";
+        //
+        // Form1
+        //
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(568, 357);
+        this.Controls.Add(this.richTextBox1);
+        this.Name = "Form1";
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        this.Text = "Form1";
+        this.Load += new System.EventHandler(this.Form1_Load);
+        this.ResumeLayout(false);
+
+    }
+
+    #endregion
+    private System.Windows.Forms.RichTextBox richTextBox1;
+}
